53cde00ced9af009cd92fb44b5b8c01407c9f41c
snehashetty05 <sneha2017shetty@gmail.com>
2022-02-08 11:41:49 +0530

    Added Controller and Swagger Configuration

---
+++ SpringBootProject/src/main/java/com/springboot/controller/ProjectController.java
@@ -0,0 +1,16 @@
+package com.springboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ProjectController {
+	
+	@GetMapping("/message")
+	public String getGreet() {
+		return "Hello World!!";
+	}
+
+}
