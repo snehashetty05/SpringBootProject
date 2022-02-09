6ab2ac6c5b0fe7b5a757a31246f02ca0affc132b
snehashetty05 <sneha2017shetty@gmail.com>
2022-02-09 16:48:44 +0530

    Autowired RestTemplate

--- SpringBootProject/src/main/java/com/springboot/SpringBootProjectApplication.java
+++ SpringBootProject/src/main/java/com/springboot/SpringBootProjectApplication.java
@@ -2,12 +2,19 @@ package com.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SpringBootProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootProjectApplication.class, args);
+	}
+	
+	@Bean
+	public RestTemplate restTemplate() {
+	    return new RestTemplate();
 	}
 
 }
--- SpringBootProject/src/main/java/com/springboot/service/implementation/IGreetingServiceImple.java
+++ SpringBootProject/src/main/java/com/springboot/service/implementation/IGreetingServiceImple.java
@@ -1,5 +1,6 @@
 package com.springboot.service.implementation;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -8,11 +9,13 @@ import com.springboot.service.IGreetingService;
 
 @Service
 public class IGreetingServiceImple implements IGreetingService {
+	
+	@Autowired
+	private RestTemplate restTemplate;
 
 	@Override
 	public String getGreetingMessage() {
 		String uri = "https://httpbin.org/uuid";
-		RestTemplate restTemplate = new RestTemplate();
 		Greeting result = restTemplate.getForObject(uri, Greeting.class);
 		String ans = result.getUuid();
 		return ans;
